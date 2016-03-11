91cc991a0f06b936ad766516643d4b8833873352
Takeshi Ogasawara <takeshi@jp.ibm.com>
2016-03-11 18:36:31 +0900

    fixed typo

--- build.sh
+++ build.sh
@@ -65,4 +65,4 @@ cd ../samtools
 mkdir filter.d accelerator.d
 ln -s ${PWD}/../prefilter/lib_pre_filter.so filter.d/
 ln -s ${PWD}/../sort_by_coordinate/lib_sort_by_coordinate.so filter.d/
-ln -s ${PWD}/../hw_zlib/lib_hw_zlib.so accelerator.d/
+ln -s ${PWD}/../hw_zlib/libz_hw.so accelerator.d/
